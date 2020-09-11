9a1bfa1c92bef85c34daf00627395122bee55828
Zachary Durr <zdurr22@gmail.com>
2020-09-11 23:09:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]}  | #{board[1]}  | #{board[3]}  \n"
   puts "-----------\n"
-  puts " #{board[4]}  | #{board[5]} | #{board[6]}  \n"
+  puts " #{board[4]}  | #{board[5]}  | #{board[6]}  \n"
   puts "-----------\n"
   puts " #{board[7]}  | #{board[8]}  | #{board[9]}  \n"
 end
